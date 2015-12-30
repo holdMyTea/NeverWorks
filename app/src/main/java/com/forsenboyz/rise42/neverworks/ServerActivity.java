053f071e469d2f489c56e32c8971a7f83b4cffff
rise42 <makedonskiy2500@gmail.com>
2015-12-30 22:57:38 +0200

    ListView works

--- app/src/main/java/com/forsenboyz/rise42/neverworks/ServerActivity.java
+++ app/src/main/java/com/forsenboyz/rise42/neverworks/ServerActivity.java
@@ -3,11 +3,12 @@ package com.forsenboyz.rise42.neverworks;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-/* I Used desktop version instead
-    https://github.com/rise42/NeverDesktop
-    */
+    //I Used desktop version instead
+
+    //  https://github.com/rise42/NeverDesktop
 
 public class ServerActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
